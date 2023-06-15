f257e5c372bf30e58a67876f0eb4ca29c629f8e6
KanaiGrace <gracemburu978@gmail.com>
2023-06-15 14:38:27 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point and prints out a sentence
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,");
+
+	return (0);
+}
