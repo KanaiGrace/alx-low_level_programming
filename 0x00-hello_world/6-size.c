3433b5856b06739d105b62caa74960bf67cd6982
KanaiGrace <gracemburu978@gmail.com>
2023-06-15 16:39:18 -0700

    trzy

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-printf("Size of char: %zu\n byte(s)", sizeof(char));
-printf("Size of int: %zu\n byte(s)", sizeof(int));
-printf("Size of long int: %zu\n byte(s)", sizeof(long int));
-printf("Size of long long int: %zu\n byte(s)", sizeof(long long int));
-printf("Size of float: %zu\n byte(s)", sizeof(float));
+printf("Size of char: %zu byte(s\n)", sizeof(char));
+printf("Size of int: %zu byte(s)\n", sizeof(int));
+printf("Size of long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of float: %zu byte(s)\n", sizeof(float));
 return (0);
 }
 
