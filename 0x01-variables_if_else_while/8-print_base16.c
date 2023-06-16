5687036fdc88f0170aff9e0c976f779515375ab0
KanaiGrace <gracemburu978@gmail.com>
2023-06-16 15:17:25 -0700

    add

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints hexadecimal
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int b;
+
+for (b = 0; b < 16; b++)
+{
+if (b < 10)
+{
+putchar(b + '0');
+}
+else
+{
+putchar(b - 10 + 'A');
+}
+}
+putchar('\n');
+return (0);
+}
