df8df59ff47caf4b2da9778c60dc750bb9e210d6
KanaiGrace <gracemburu978@gmail.com>
2023-06-16 15:55:18 -0700

    number9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int c;
+
+for (c = 0; c < 10; c++)
+{
+putchar(c + '0');
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
