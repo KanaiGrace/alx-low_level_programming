7fe1f5c098579ed32adb8ad672b6065637bf5924
KanaiGrace <gracemburu978@gmail.com>
2023-06-17 03:58:31 -0700

    digits1

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,18 +12,18 @@ int a;
 int b;
 
 int c;
-for  (a = 0; a < 9; a++)
+for  (a = '0'; a < '9'; a++)
 {
-for (b = a + 1; b <= 9; b++)
+for (b = a + 1; b <= '9'; b++)
 {
-for (c = b + 1; c <= 9; c++)
+for (c = b + 1; c <= '9'; c++)
 {
 if ((b != a) != c)
 {
 putchar(a);
 putchar(b);
 putchar(c);
-if (a == 7 && b == 8 && c == 9)
+if (a == '7' && b == '8' && c == '9')
 putchar(',');
 putchar(' ');
 }
