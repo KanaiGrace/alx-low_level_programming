de075a53d7ed8caef3b2618c21aae9944bda3df2
KanaiGrace <gracemburu978@gmail.com>
2023-06-17 02:50:17 -0700

    number10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - prints several numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int a;
+
+int b;
+for (a = '0'; a < 9; a++)
+{
+for (b = a + 1; b < 9; b++)
+{
+if (a != b)
+{
+putchar(a);
+putchar(b);
+if (a == '8' && b == '9')
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
