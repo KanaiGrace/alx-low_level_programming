e346edb360f299f09e1d5d655d36285a435bd3b8
KanaiGrace <gracemburu978@gmail.com>
2023-06-20 14:54:01 -0700

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - prints numebrs
+ *@n :value
+ *
+ * Return: void
+ */
+void print_to_98(int n)
+{
+for (n = 0; n <= 98; n++)
+{
+printf("%d, ", n);
+}
+}
