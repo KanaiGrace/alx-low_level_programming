fb901f3f08366d20e846546b76ea43d96440e74c
KanaiGrace <gracemburu978@gmail.com>
2023-06-20 05:43:33 -0700

    push custom implemnt

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,9 +9,14 @@
 
 int main(void)
 {
-_putchar('b');
-_putchar('e');
-_putchar('d');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 return (0);
 }
