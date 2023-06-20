42aadea9c4354b1b47e838e387bffc84b1b5db12
KanaiGrace <gracemburu978@gmail.com>
2023-06-20 14:21:48 -0700

    table times

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,47 +7,20 @@
  */
 void times_table(void)
 {
-int a, b, c, d, e, f, g, h, i, j;
+int a, b, result;
 
-for (a = 0; a < 9; a++)
+for (a = 0; a <= 9; a++)
 {
 for (b = 0; b <= 9; b++)
 {
-for (c = 0; c <= 18; c += 2)
+result = a * b;
+_putchar(result + '0');
+if (b != 9)
 {
-for (d = 0; d <= 27; d += 3)
-{
-for (e = 0; e <= 36; e += 4)
-{
-for (f = 0; f <= 45; f += 5)
-{
-for (g = 0; g <= 54; g += 6)
-{
-for (h = 0; h <= 63; h += 7)
-{
-for (i = 0; i <= 72; i += 8)
-{
-for (j = 0; j <= 81; j += 9)
-{
-_putchar(a);
-_putchar(b);
-_putchar(c);
-_putchar(d);
-_putchar(e);
-_putchar(f);
-_putchar(g);
-_putchar(h);
-_putchar(i);
-_putchar(j);
+_putchar(',');
+_putchar(' ');
 }
 }
+_putchar('\n');
 }
 }
-}
-}
-}
-}
-}
-}
-}
-
