451bb43423466ba53b619f5365a6bcd921b3bf37
KanaiGrace <gracemburu978@gmail.com>
2023-06-20 15:22:02 -0700

    comit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,8 +9,21 @@
  */
 void print_to_98(int n)
 {
-for (n = 0; n <= 98; n++)
+if (n <= 98)
+{
+for (n = 0; n < 98; n++)
 {
 printf("%d, ", n);
 }
 }
+else
+{
+for (n = 0; n > 98; n--)
+{
+printf("%d, ", n);
+}
+}
+printf("%d\n", n);
+}
+
+
