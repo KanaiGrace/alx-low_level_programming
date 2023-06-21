37a9f38cfa77b1a0d25e09a009de2fb02024217c
KanaiGrace <gracemburu978@gmail.com>
2023-06-21 13:58:38 -0700

    5th debug

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * positive_or_negative - Prints output indicating positive negative or zero
+ *
+ * @i: holds value
+ * Return: 0
+ */
+void positive_or_negative(int i)
+{
+if (i > 0)
+{
+printf("%d is positive\n", i);
+}
+else if (i == 0)
+{
+printf("%d is zero\n", i);
+}
+else if (i < 0)
+{
+printf("%d is negative\n", i);
+}
+}
