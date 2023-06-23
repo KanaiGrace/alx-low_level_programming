01f58f7721ee1d1b51afec3cc8a56ebc58141d67
KanaiGrace <gracemburu978@gmail.com>
2023-06-23 15:21:17 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints more numbers
+ *
+ * Return: 0
+ */
+void more_numbers(void)
+{
+int j;
+
+int i;
+for (i = 0; i <= 10; i++)
+{
+for (j = 0; j <= 14; j++)
+{
+if (j > 9)
+_putchar(j / 10 + '0');
+_putchar(j % 10 + '0');
+}
+putchar('\n');
+}
+}
