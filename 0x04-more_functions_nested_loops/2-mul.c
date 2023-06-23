b2793e79bffe9942424c3ea42837b90bf7789aca
KanaiGrace <gracemburu978@gmail.com>
2023-06-23 04:47:12 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * mul - performs multiplication
+ * @a: holds value
+ * @b: holds value
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+int product;
+product = a * b;
+return (product);
+}
---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - prints character to standard output
+ *
+ * Return: character written else -1
+ */
+int _putchar(char c)
+{
+return write (1, &c, 1);
+}
