cb56d6b0506296c0c93eaa24731ddb86b5042c79
KanaiGrace <gracemburu978@gmail.com>
2023-06-27 11:26:07 -0700

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - print second half of string
+ * @str: holds value
+ * return: void
+ */
+void puts_half(char *str)
+{
+int length = strlen(str);
+int i;
+if ((length % 2 == 0) || ((length - 1) % 2 == 0))
+{
+for (i = length / 2; i < length; i++)
+{
+_putchar(str[i]);
+}
+}
+_putchar('\n');
+}
+
