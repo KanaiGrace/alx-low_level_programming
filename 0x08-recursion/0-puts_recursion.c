7ca8ecd0319192b54592f1dc45669d2c24369d06
KanaiGrace <gracemburu978@gmail.com>
2023-07-05 14:17:22 -0700

    puts recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - calls puts function
+ * @s: pointer to string
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+puts(s);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+Recursion whereby a function calls itself
