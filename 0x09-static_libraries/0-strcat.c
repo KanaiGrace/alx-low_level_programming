0a1239c86741a51c20e99893f58263bfce255cac
KanaiGrace <gracemburu978@gmail.com>
2023-07-08 07:32:12 -0700

    libmy.a

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - concatenates strings
+ * @dest: pointer to destination
+ * @src: pointer to source
+ * Return: pointer to resulting dest
+ */
+char *_strcat(char *dest, char *src)
+{
+strcat(dest, src);
+return (dest);
+}
---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _islower - checks for lowercase
+ *
+ * @c: character to be checked
+ * Return: 0 or 1
+ */
+int _islower(int c)
+{
+if (islower(c))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+return (0);
+}
