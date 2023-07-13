90bc64c211e5349b685c9a8e9988fe365f9b9b8d
KanaiGrace <gracemburu978@gmail.com>
2023-07-13 06:01:32 -0700

    concate2

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,25 +17,17 @@ s1 = "";
 if (s2 == NULL)
 s2 = "";
 while (s1[len1] != '\0')
-{
 len1++;
-}
 while (s2[len2] != '\0')
-{
 len2++;
-}
 if (len2 < n)
 {
 totalen = len1 + n;
 z = malloc(sizeof(char) * (totalen + 1));
 if (z == NULL)
-{
 return (NULL);
-}
 for (i = 0; i < len1; i++)
-{
 z[i] = s1[i];
-}
 for (j = 0; j < len2; j++)
 z[i + j] = s2[j];
 z[i + j] = '\0';
@@ -45,9 +37,7 @@ else
 totalen = len1 + len2;
 z = malloc(sizeof(char) * (len1 + len2 + 1));
 if (z == NULL)
-{
 return (NULL);
-}
 for (i = 0; i < len1; i++)
 z[i] = s1[i];
 for (j = 0; j < n; j++)
