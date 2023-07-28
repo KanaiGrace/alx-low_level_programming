36909a67f5fb0d02975e95911ba5e663c22c2f2d
KanaiGrace <gracemburu978@gmail.com>
2023-07-28 06:07:53 -0700

    task0repeat

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,9 +14,9 @@ x = 0;
 while (h)
 {
 if (!h->str)
-printf("[0](nil)\n");
+printf("[0] (nil)\n");
 else
-printf("[%u]%s\n", h->len, h->str);
+printf("[%u] %s\n", h->len, h->str);
 h = h->next;
 x++;
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -19,5 +19,5 @@ struct list_s *next;
 
 size_t print_list(const list_t *h);
 list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
+
 #endif /* LISTS_H */
