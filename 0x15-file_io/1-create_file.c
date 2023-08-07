072008a4c293eabaa013570fe276e5d44b15f5ac
KanaiGrace <gracemburu978@gmail.com>
2023-08-07 11:58:18 -0700

    rep2 task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,17 +10,23 @@
 int create_file(const char *filename, char *text_content)
 {
 int fld, hld, count;
+count = 0;
 if (filename == NULL)
+return (-1);
+fld = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+if (fld == -1)
 return (-1);
 if (text_content != NULL)
 {
-for (count = 0; text_content[count];)
+while (text_content[count] != '\0')
 count++;
-}
-fld = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 hld = write(fld, text_content, count);
-if (fld == -1 || hld == -1)
+if (hld == -1)
+{
+close(fld);
 return (-1);
+}
+}
 close(fld);
 return (1);
 }
