27101f6083acec8d8a44faf989c74e8c937f9388
KanaiGrace <gracemburu978@gmail.com>
2023-08-07 07:08:20 -0700

    rep task0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,10 +15,10 @@ int a;
 size_t cnt = 0;
 ssize_t wtch;
 char *buffer;
-ptrfile = fopen(filename, "r");
+ptrfile = fopen(filename, "r+" );
 if (ptrfile == NULL)
 {
-return (-1);
+return (0);
 }
 buffer = (char *)malloc((letters + 1) * sizeof(char));
 if (buffer == NULL)
@@ -29,10 +29,10 @@ buffer[cnt] = a;
 cnt++;
 }
 buffer[cnt] = '\0';
-fclose(ptrfile);
 wtch = write(STDOUT_FILENO, buffer, cnt);
 if (wtch == -1 || wtch < (ssize_t)cnt)
-return (-1);
+return (0);
 free(buffer);
+fclose(ptrfile);
 return (cnt);
 }
